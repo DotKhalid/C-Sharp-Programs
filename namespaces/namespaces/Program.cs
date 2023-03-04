9b0b42cf2c83cb2c251b30ffde84b989f28f9b73
dotkhalid <mdotkhalid4@gmail.com>
2023-03-04 16:17:10 +0500

    first commit

---
+++ namespaces/namespaces/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using namespaces.people;
+
+namespace namespaces
+{
+    class Program
+    {     
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            person.name = "ali";
+            person.age = 34;
+            person.email = "ali@gmail.com";
+
+            person.greeting();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ namespaces/namespaces/people/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespaces.people
+{
+    public class Person
+    {
+        public string name;
+        public string email;
+        public int age;
+
+        public void greeting()
+        {
+            System.Console.WriteLine("Hello {0} verified your age {1} from {2} ", name, age, email);
+        }
+
+    }
+}
